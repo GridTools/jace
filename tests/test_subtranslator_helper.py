3ebbc3639ce80e97c02a61ceb5faad3aa6b923c6
Philip Mueller <philip.mueller@cscs.ch>
2024-04-22 13:37:31 +0200

    Updated the tests for the subtranslator helper.

--- tests/test_subtranslator_helper.py
+++ tests/test_subtranslator_helper.py
@@ -12,6 +12,8 @@ from __future__ import annotations
 from collections.abc import Collection
 from typing import Any
 
+import pytest
+
 from jace import translator as jtrans
 
 
@@ -147,17 +149,20 @@ def test_subtranslatior_order_custom1():
 
 
 def test_subtranslatior_managing():
-    """Esnsures the functionality of the subtranslator managing."""
+    """Ensures the functionality of the subtranslator managing."""
     from jace.translator.sub_translators import (
         _get_subtranslators_cls,
         add_subtranslator,
+        rm_subtranslator,
     )
 
     class ValidSubTrans(jtrans.JaCeSubTranslatorInterface):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
 
-    #
+    class ValidSubTrans2(jtrans.JaCeSubTranslatorInterface):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
 
     class InvalidSubTrans:
         def __init__(self): ...
@@ -200,23 +205,63 @@ def test_subtranslatior_managing():
     #
 
     # Test the initial conditions
-    init_sub_trans_list = _get_subtranslators_cls(builtins=False)
-    init_built_in = _get_subtranslators_cls(with_external=False)  # noqa: F841  # Not finished
+    builtin_subtrans = _get_subtranslators_cls(with_external=False)
+    curr_external_subtrans = _get_subtranslators_cls(builtins=False)
+    exp_curr_external_subtrans = []
     assert (
-        len(init_sub_trans_list) == 0
-    ), f"Expected no external subtranslators but found: {init_sub_trans_list}"
+        curr_external_subtrans == exp_curr_external_subtrans
+    ), f"Expected no external subtranslators but found: {builtin_subtrans}"
+    assert (
+        len(builtin_subtrans) != 0
+    ), "Expected to have some builtin subtranslator, but there were none."
+    assert builtin_subtrans is not _get_subtranslators_cls()  # Ensures no sharing
 
-    # Now we add the valid subtranslator interface
-    assert add_subtranslator(ValidSubTrans), "Failed to add the `ValidSubTrans`"
-    first_sub_trans = _get_subtranslators_cls(builtins=False)  # noqa: F841  # Not finished
+    # Add a subtranslator to the internal list
+    assert add_subtranslator(ValidSubTrans), "Failed to add 'ValidSubTrans'"
+    exp_curr_external_subtrans = [ValidSubTrans]
+    curr_external_subtrans = _get_subtranslators_cls(builtins=False)
+    assert (
+        curr_external_subtrans == exp_curr_external_subtrans
+    ), f"Wrong subtranslator order, expected '{exp_curr_external_subtrans}' got '{curr_external_subtrans}'."
+    assert builtin_subtrans == _get_subtranslators_cls(with_external=False)
+    assert _get_subtranslators_cls() == exp_curr_external_subtrans + builtin_subtrans
 
-    # Should not include the
-    subTrans = _get_subtranslators_cls(with_external=False)  # noqa: F841  # Not finished
+    # Add a second translator
+    assert add_subtranslator(ValidSubTrans2), "Failed to add 'ValidSubTrans2'"
+    exp_curr_external_subtrans = [ValidSubTrans2, ValidSubTrans]  # FILO order
+    curr_external_subtrans = _get_subtranslators_cls(builtins=False)
+    assert (
+        exp_curr_external_subtrans == curr_external_subtrans
+    ), f"Wrong subtranslator order, expected '{exp_curr_external_subtrans}' got '{curr_external_subtrans}'."
+    assert exp_curr_external_subtrans + builtin_subtrans == _get_subtranslators_cls()
 
-    assert not add_subtranslator(ValidSubTrans), "Could add `ValidSubTrans` twice"
-    raise AssertionError("NOT FINISHED YET")
+    # Now we try to add some translators that will be rejected.
+    assert not add_subtranslator(ValidSubTrans)  # Already known
+    assert not add_subtranslator(ValidSubTrans2)  # Already known
+    assert not add_subtranslator(ValidSubTrans())  # Is an instance
+    assert not add_subtranslator(InvalidSubTrans)  # Not implementing interface
+    assert exp_curr_external_subtrans + builtin_subtrans == _get_subtranslators_cls()
+
+    # Now we remove a translator from the list.
+    assert rm_subtranslator(ValidSubTrans), "Failed to remove 'ValidSubTrans'"
+    exp_curr_external_subtrans = [ValidSubTrans2]
+    curr_external_subtrans = _get_subtranslators_cls(builtins=False)
+    assert (
+        curr_external_subtrans == exp_curr_external_subtrans
+    ), f"Wrong subtranslator order, expected '{exp_curr_external_subtrans}' got '{curr_external_subtrans}'."
+    assert builtin_subtrans == _get_subtranslators_cls(with_external=False)
+    assert _get_subtranslators_cls() == exp_curr_external_subtrans + builtin_subtrans
+
+    # Now try to remove it again.
+    assert not rm_subtranslator(ValidSubTrans), "Was allowed to remove 'ValidSubTrans' again!"
+    with pytest.raises(
+        expected_exception=KeyError, match=f"Subtranslator '{type(ValidSubTrans)}' is not known."
+    ):
+        rm_subtranslator(ValidSubTrans, strict=True)
+    #
 
 
 if __name__ == "__main__":
     test_subtranslatior_order_simple()
     test_subtranslatior_order_custom1()
+    test_subtranslatior_managing()
