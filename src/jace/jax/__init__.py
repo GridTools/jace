11a2f57ca8636bfe4852ff9564422b12255d413d
Philip Mueller <philip.mueller@cscs.ch>
2024-04-26 08:41:35 +0200

    Started with a `jace.jax` package that can be used as a better drop in replacement.
    I think that we will need it in the future, for example it is, in my view, currently the best place to put `jace.jit`.
    Another idea that could be worthwhile to consider is to mimick the `jace` package after `jax`.

---
+++ src/jace/jax/__init__.py
@@ -0,0 +1,8 @@
+# JaCe - JAX Just-In-Time compilation using DaCe (Data Centric Parallel Programming)
+#
+# Copyright (c) 2024, ETH Zurich
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""This package mimics parts of the interface of the `jax` package that is supported by JaCe."""
