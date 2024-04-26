97a35beaeffb9f873484803c436f834f58d85686
Philip Mueller <philip.mueller@cscs.ch>
2024-04-26 15:35:32 +0200

    WIP: Started with a caching mechanism for teh Jiting.
    However, I have to think more.
    There are some things that I do not like, especially that we have to chache the "abstract input data" but must also use value.
    
    I think that this should be solved a bit differently.

--- src/jace/jax/api_helper.py
+++ src/jace/jax/api_helper.py
@@ -9,9 +9,14 @@
 
 from __future__ import annotations
 
+from functools import lru_cache
 from typing import Any
 
+import dace
+import jax
+
 from jace import util as jutil
+from jace.translator import util as jtrutil
 
 
 class JitWrapped:
@@ -34,6 +39,7 @@ class JitWrapped:
         """Creates a wrapped jace jitable object of `jax_prim`."""
         assert jax_prim is not None
         self._fun = jax_prim
+        self._tran_count = 0
 
     def __call__(
         self,
@@ -76,9 +82,116 @@ class JitWrapped:
         """
         from jace.translator.util import debug as jtrudebug
 
-        return jtrudebug._jace_run(self._fun, *args, **kwargs)
+        memento: jtrutil.JaCeTranslationMemento = self._get_memento(*args, **kwargs)
+        return jtrudebug.run_memento(memento, *args)
+
+    def _get_memento(
+        self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> jtrutil.JaCeTranslationMemento:
+        """This function returns the Memento.
+
+        The function will transform its arguments into `_ArgInfo` versions.
+        This is needed since Jax only cares about the information stored inside it.
+        The positional only arguments are used to cache the settings important for Jax
+        and the kwonly arguments are used to influence the Jaxpr to SDFG translator.
+
+        Notes:
+            It is forbidden to permanently modify the returned memento.
+                Doing so results in undefined behaviour.
+        """
+        return self._get_memento_cached(
+            *(_ArgInfo.from_value(v) for v in args),
+            **kwargs,
+        )
+
+    @lru_cache
+    def _get_memento_cached(
+        self,
+        *args: _ArgInfo,
+        **kwargs: Any,
+    ) -> jtrutil.JaCeTranslationMemento:
+        """Generates the SDFG from
+
+        Todo:
+            Also make the SDFG compiled and permanent also in the memento
+            Implement a better cache that avoids using this strange way to pass values around.
+
+        Notes:
+            It is forbidden to permanently modify the returned memento.
+                Doing so results in undefined behaviour.
+        """
+        from jace.translator import JaxprTranslationDriver
+
+        real_args: tuple[Any, ...] = tuple(x._get_val_once() for x in args)
+        jaxpr = jax.make_jaxpr(self.__wrapped__)(*real_args)
+        driver = JaxprTranslationDriver(**kwargs)
+        return driver.translate_jaxpr(jaxpr)
 
     @property
     def __wrapped__(self) -> Any:
         """Returns the wrapped object."""
         return self._fun
+
+    def __hash__(self) -> int:
+        """Hash based on the wrapped function (needed for caching)."""
+        return hash(self.__wrapped__)
+
+    def __eq__(self, other: Any) -> bool:
+        """Wrapped function based equality testing (needed for caching)."""
+        if not isinstance(other, JitWrapped):
+            return False
+        return self.__wrapped__ == other.__wrapped__
+
+
+class _ArgInfo:
+    """Abstracts argument for the case of the `JitWrapped` object.
+
+    Essentially represents a single argument.
+    To construct it use the `from_value()` function.
+
+    Notes:
+        An `_ArgInfo` instance also keeps a reference to the value that was used to construct it.
+            However this value can only retrieved once and is removed afterwards.
+            Conceptionally it should be a weak reerence, but several classes (especially `int`
+            and `float` can not be weakly referenced.
+    """
+
+    shape: tuple[int, ...]
+    strides: tuple[int, ...]
+    dtype: dace.typeclass
+    location: dace.StorageType  # We only need CPU and GPU.
+    _val: Any | None  # May not be allocated.
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """To construct an `_ArgInfo` instance use `from_val()`."""
+        raise NotImplementedError("Use '_ArgInfo.from_value()' to construct an instance.")
+
+    def __hash__(self) -> int:
+        return hash((self.shape, self.strides, self.dtype, self.location))
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, _ArgInfo):
+            return False
+        return (self.shape, self.strides, self.dtype, self.location) == (
+            (other.shape, other.strides, other.dtype, other.location)
+        )
+
+    def _get_val_once(self) -> Any:
+        """Returns the wrapped object.
+
+        This function only works for a single time.
+        Calling it will null the reference of `self`.
+        """
+        if self._val is None:
+            raise RuntimeError("Value was already consumed.")
+        val = self._val
+        self._val = None
+        return val
+
+    @classmethod
+    def from_value(cls, val: Any) -> _ArgInfo:
+        """Constructs an `_ArgInfo` instance from `val`."""
+        arginfo: _ArgInfo = cls.__new__(cls)
+        raise NotImplementedError("'_ArgInfo.from_value()' is not implemented.")
