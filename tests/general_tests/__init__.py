5bb4b5e557906af0855e95e7bc23d0509e28578a
Philip Mueller <philip.mueller@cscs.ch>
2024-05-29 13:35:37 +0200

    Started to reorganize the tests.

---
+++ tests/general_tests/__init__.py
@@ -0,0 +1,8 @@
+# JaCe - JAX Just-In-Time compilation using DaCe (Data Centric Parallel Programming)
+#
+# Copyright (c) 2024, ETH Zurich
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""General Jace tests."""
