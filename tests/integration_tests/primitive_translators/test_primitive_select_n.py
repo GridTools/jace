8bdea8238850dd7e7d7dee1753a158ed90a039ab
Philip Mueller <philip.paul.mueller@bluemain.ch>
2024-06-01 14:34:37 +0200

    Compacted the tests for select.

--- tests/integration_tests/primitive_translators/test_primitive_select_n.py
+++ tests/integration_tests/primitive_translators/test_primitive_select_n.py
@@ -21,39 +21,27 @@ import jace
 from tests import util as testutil
 
 
-@pytest.fixture()
-def Pred() -> np.ndarray:
-    return testutil.mkarray((10, 10)) > 0.5
-
-
-@pytest.fixture()
-def tbranch() -> np.ndarray:
-    return np.ones((10, 10))
-
-
-@pytest.fixture()
-def fbranch() -> np.ndarray:
-    return np.zeros((10, 10))
-
-
 def _perform_test(testee: Callable, *args: Any):
 
     res = testee(*args)
     ref = jace.jit(testee)(*args)
-
-    assert args[0].shape == res.shape
     assert np.all(res == ref)
 
 
-def test_select_n_where(Pred, tbranch, fbranch):
+def test_select_n_where():
     """Normal `np.where` test."""
 
     def testee(P: Any, T: Any, F: Any) -> Any:
         return jnp.where(P, T, F)
-    _perform_test(testee, Pred, tbranch, fbranch)
+
+    shape = (10, 10)
+    pred = testutil.mkarray(shape, np.bool_)
+    tbranch = testutil.mkarray(shape)
+    fbranch = testutil.mkarray(shape)
+    _perform_test(testee, pred, tbranch, fbranch)
 
 
-def test_select_n_where_one_literal(Pred, tbranch, fbranch):
+def test_select_n_where_literal():
     """`np.where` where one of the input is a literal.
     """
 
@@ -63,32 +51,27 @@ def test_select_n_where_one_literal(Pred, tbranch, fbranch):
     def testee2(P: Any, T: Any) -> Any:
         return jnp.where(P, T, 3)
 
-    _perform_test(testee1, Pred, fbranch)
-    _perform_test(testee2, Pred, tbranch)
-
-
-def test_select_n_where_full_literal(Pred):
-    """`np.where` where all inputs are literals."""
-
-    def testee(P: Any) -> Any:
+    def testee3(P: Any) -> Any:
         return jnp.where(P, 8, 9)
 
-    # If not a scalar, Jax will do broadcasting and no literal substitution is done.
-    Pred = Pred[0, 0]
-    _perform_test(testee, Pred)
+    shape = ()
+    pred = testutil.mkarray(shape, np.bool_)
+    tbranch = testutil.mkarray(shape, np.int_)
+    fbranch = testutil.mkarray(shape, np.int_)
+
+    _perform_test(testee1, pred, fbranch)
+    _perform_test(testee2, pred, tbranch)
+    _perform_test(testee3, pred)
 
 
 def test_select_n_many_inputs():
     """Tests the generalized way of using the primitive."""
-    nbcases = 10
-    shape = (10, 10)
-    cases = [np.full(shape, i) for i in range(nbcases)]
-    pred = np.arange(cases[0].size).reshape(shape) % nbcases
 
     def testee(pred: np.ndarray, *cases: np.ndarray) -> jax.Array:
         return jax.lax.select_n(pred, *cases)
 
-    ref = testee(pred, *cases)
-    res = jace.jit(testee)(pred, *cases)
-
-    assert np.all(ref == res)
+    nbcases = 10
+    shape = (10, 10)
+    cases = [np.full(shape, i) for i in range(nbcases)]
+    pred = np.arange(cases[0].size).reshape(shape) % nbcases
+    _perform_test(testee, pred, *cases)
