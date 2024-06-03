fa599c399a7de57ef666b51cb45e32f66103241c
Philip Mueller <philip.mueller@cscs.ch>
2024-06-03 08:16:19 +0200

    Added some more tests to the empty case.

--- tests/integration_tests/test_empty_jaxpr.py
+++ tests/integration_tests/test_empty_jaxpr.py
@@ -22,33 +22,67 @@ import jace
 
 def test_empty_array():
     @jace.jit
-    def testee(A: np.ndarray) -> np.ndarray:
+    def wrapped(A: np.ndarray) -> np.ndarray:
         return A
 
     A = np.arange(12, dtype=np.float64).reshape((4, 3))
+    res = wrapped(A)
 
-    assert np.all(testee(A) == A)
+    assert np.all(res == A)
+    assert res.__array_interface__["data"][0] != A.__array_interface__["data"][0]
+
+
+def test_empty_multiple():
+    @jace.jit
+    def wrapped(A: np.ndarray, B: np.float64) -> tuple[np.ndarray, np.float64]:
+        return A, B
+
+    A = np.arange(12, dtype=np.float64).reshape((4, 3))
+    B = np.float64(30.0)
+    res = wrapped(A, B)
+
+    assert np.all(res[0] == A)
+    assert res[1] == B
+    assert res[0].__array_interface__["data"][0] != A.__array_interface__["data"][0]
+
+
+def test_empty_unused():
+    @jace.jit
+    def wrapped(A: np.ndarray, B: np.float64) -> np.ndarray:  # noqa: ARG001  # Explicitly unused.
+        return A
+
+    A = np.arange(12, dtype=np.float64).reshape((4, 3))
+    B = np.float64(30.0)
+    lowered = wrapped.lower(A, B)
+    compiled = lowered.compile()
+    res = compiled(A, B)
+
+    assert len(lowered._translated_sdfg.inp_names) == 2
+    assert len(compiled._inp_names) == 2
+    assert isinstance(res, np.ndarray)
+    assert np.all(res == A)
+    assert res.__array_interface__["data"][0] != A.__array_interface__["data"][0]
 
 
 def test_empty_scalar():
     @jace.jit
-    def testee(A: float) -> float:
+    def wrapped(A: float) -> float:
         return A
 
     A = np.pi
 
-    assert np.all(testee(A) == A)
+    assert np.all(wrapped(A) == A)
 
 
 @pytest.mark.skip(reason="Nested Jaxpr are not handled.")
 def test_empty_nested():
     @jace.jit
-    def testee(A: float) -> float:
+    def wrapped(A: float) -> float:
         return jax.jit(lambda A: A)(A)
 
     A = np.pi
 
-    assert np.all(testee(A) == A)
+    assert np.all(wrapped(A) == A)
 
 
 def test_empty_with_drop_vars():
@@ -56,12 +90,12 @@ def test_empty_with_drop_vars():
 
     @jace.jit
     @jace.grad
-    def testee(A: float) -> float:
+    def wrapped(A: float) -> float:
         return A * A
 
     A = np.pi
 
-    assert np.all(testee(A) == 2.0 * A)
+    assert np.all(wrapped(A) == 2.0 * A)
 
 
 @pytest.mark.skip(reason="Literal return value is not implemented.")
@@ -72,13 +106,16 @@ def test_empty_literal_return():
         Using this test function serves another purpose. Since Jax includes the original
         computation in the Jaxpr coming from a `grad` annotated function, the result will have
         only drop variables.
+
+    Todo:
+        Add a test if we really have a literal return value, but for that we need the Jaxpr.
     """
 
     @jace.jit
     @jace.grad
-    def testee(A: float) -> float:
+    def wrapped(A: float) -> float:
         return A + A + A
 
     A = np.e
 
-    assert np.all(testee(A) == 3.0)
+    assert np.all(wrapped(A) == 3.0)
