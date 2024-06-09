c5ebe96fee1108a6b06ffa5cf6be2c98e0c4722e
Philip Mueller <philip.paul.mueller@bluemain.ch>
2024-06-09 11:14:43 +0200

    Renamed a file.

--- src/jace/util/__init__.py
+++ src/jace/util/__init__.py
@@ -9,6 +9,7 @@
 
 from __future__ import annotations
 
+from .definitions import FORBIDDEN_SDFG_VAR_NAMES, VALID_SDFG_OBJ_NAME, VALID_SDFG_VAR_NAME
 from .jax_helper import (
     JaCeVar,
     get_jax_var_dtype,
@@ -18,7 +19,6 @@ from .jax_helper import (
     propose_jax_name,
     translate_dtype,
 )
-from .misc import FORBIDDEN_SDFG_VAR_NAMES, VALID_SDFG_OBJ_NAME, VALID_SDFG_VAR_NAME
 from .traits import (
     is_array,
     is_drop_var,
