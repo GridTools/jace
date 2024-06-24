67e546c28043c5dc423850b5bbeb752b9e53106f
Philip Mueller <philip.mueller@cscs.ch>
2024-06-24 13:12:23 +0200

    Added the functionality to the `pjit` translator to handle literal inputs.
    They are translated to a constant and then feeded in.
    I did not use a Tasklet, because this could block inline.
    However, this also blocks literal substitution.

--- src/jace/translator/primitive_translators/pjit_translator.py
+++ src/jace/translator/primitive_translators/pjit_translator.py
@@ -15,7 +15,7 @@ from typing import TYPE_CHECKING, Any
 
 from jax._src import sharding_impls as jax_sharding  # noqa: PLC2701 [import-private-name]
 
-from jace import translator
+from jace import translator, util
 from jace.translator import post_translation as ptranslation
 
 
@@ -43,6 +43,8 @@ def PJITTranslator(  # noqa: N802 [invalid-function-name]
     However, due to the current state and working of JaCe, this aspect is essentially
     ignored and the computation is always inlined.
 
+    In case an input is a literal the translator will create a constant for it.
+
     Args:
         builder: The builder object of the translation.
         in_var_names: Names of the SDFG variables that should be used as inputs
@@ -60,15 +62,13 @@ def PJITTranslator(  # noqa: N802 [invalid-function-name]
     _ = params["keep_unused"]
     _ = params["inline"]
 
-    # TODO(phimuell): Controlflow region and name
-    pjit_name = params["name"]
-
     if not all(in_sharding is jax_sharding.UNSPECIFIED for in_sharding in in_shardings):
         raise NotImplementedError("Currently 'pjit' does not support sharding in its input.")
     if not all(out_sharding is jax_sharding.UNSPECIFIED for out_sharding in out_shardings):
         raise NotImplementedError("Currently 'pjit' does not support sharding in its output.")
-    if any(in_var_name is None for in_var_name in in_var_names):
-        raise NotImplementedError("Literal inputs to 'pjit' are not implemented.")
+
+    # TODO(phimuell): Controlflow region and name
+    pjit_name = params["name"]
 
     # TODO(phimuell): Controlflow region and name
     #  They will introduce a feature like that to address them in optimizations.
@@ -76,6 +76,28 @@ def PJITTranslator(  # noqa: N802 [invalid-function-name]
 
     # Name in SDFG must be unique, thus we mangle it, furthermore, we have to clean it.
     sdfg_name = f"pjit_{re.subn('[^a-zA-Z0-9_]', '_', pjit_name)[0]}__{'_'.join(out_var_names)}"
+
+    # If needed turn literal inputs into constants.
+    # TODO(phimuell): Is using constants really a good idea?
+    if any(in_var_name is None for in_var_name in in_var_names):
+        final_input_names: list[str] = []
+        for i, in_var_name in enumerate(in_var_names):
+            if in_var_name is None:
+                new_input_name = f"__const_{sdfg_name}_literal_input_{i}"
+                jax_input: jax_core.Atom = eqn.invars[i]
+                new_input_var = util.JaCeVar.from_atom(
+                    jax_var=jax_input,
+                    name=new_input_name,
+                )
+                builder.add_array(new_input_var)
+                builder.sdfg.add_constant(
+                    new_input_name,
+                    util.get_jax_literal_value(jax_input),
+                    builder.arrays[new_input_name],
+                )
+                in_var_name = new_input_name  # noqa: PLW2901 [redefined-loop-name]
+            final_input_names.append(in_var_name)
+        in_var_names = final_input_names
 
     # Now get the translated SDFG.
     nested_context: translator.TranslationContext = builder.translate_jaxpr(
