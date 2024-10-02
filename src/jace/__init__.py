e2aa76fea7064edf2e0c510f26078d54cda022aa
Philip Mueller <philip.mueller@cscs.ch>
2024-10-02 14:28:09 +0200

    Forcing a modern version of JAX.

--- src/jace/__init__.py
+++ src/jace/__init__.py
@@ -9,10 +9,16 @@
 
 from __future__ import annotations
 
+import jax
+
 import jace.translator.primitive_translators as _  # noqa: F401 [unused-import]  # Needed to populate the internal translator registry.
 
 from .__about__ import __author__, __copyright__, __license__, __version__, __version_info__
 from .api import grad, jacfwd, jacrev, jit
+
+
+if jax.version._version_as_tuple(jax.__version__) < (0, 4, 33):
+    raise ImportError(f"Require at least JAX version '0.4.33', but found '{jax.__version__}'.")
 
 
 __all__ = [
